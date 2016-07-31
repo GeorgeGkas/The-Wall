9ec72d5ef931da257e32a0fb68b19cc7b5bd9a92
GeorgeGks <GeorgeGks@users.noreply.github.com>
2016-07-31 18:21:58 +0300

    update front end

--- app/app.js
+++ app/app.js
@@ -3,7 +3,7 @@ var app = express();
 var MYSQL_db = require('./models/database/MYSQL');
 var utils = require('./models/utils/functions');
 
-var mysql = new MYSQL_db({
+/*var mysql = new MYSQL_db({
     host: 'localhost',
     user: 'root',
     password: 'thisisapassword',
@@ -14,7 +14,7 @@ mysql.connect();
 
 mysql.insert_author({
     name: 'George G. Gkasdrogkas',
-    avatar: 'none',
+    avatar: 'http://67.media.tumblr.com/fad1e6baa5c3bac246e258df4f9103f3/tumblr_mvj0dccSnC1s3lf4oo1_500.jpg',
     description: 'Web Developer & Designer',
     email: 'georgegkas@gmail.com'
 }, function() {
@@ -22,17 +22,14 @@ mysql.insert_author({
         mysql.insert_post({
             author: res[0].author_ID,
             date: utils.get_curr_date(),
-            content: 'image here',
+            content: 'http://66.media.tumblr.com/0fc8eb068e3b3505e93d531934df9ee4/tumblr_o3xgaegrev1tm0l12o1_500.jpg',
             type: 'img'
         }, function() {
-            mysql.delete_author('all', function() {
-                mysql.delete_post('8');
-                mysql.end();
-            });
+           mysql.end();
         });
     });
 });
-
+*/
 
 
 app.set('view engine', 'pug');
@@ -42,7 +39,34 @@ app.get('/', function(req, res) {
     res.render('index', {
         _FEATURE_POST_CONTENT: 'ABC',
         _LATEST_POST_CONTENT: 'DEF',
-        _OTHER_POSTS: 'GHI'
+        _OTHER_POSTS: [
+
+            {
+                post_type: 'quote',
+                post_title: 'Front-End-Challenge-Accepted--CSS-3D-Cube',
+                post_content: 'Do you like challenges? Are you willing to take on a task that you’ve never come across before, and do it under a deadline? What if, in carrying out the task, you encounter a problem that appears unsolvable? I want to share my experience of using CSS 3D effects for the first time in a real project and to inspire you to take on challenges. ',
+                post_date: '2016-07-31 14:16:15',
+                post_like_count: 376,
+                post_comment_count: 200,
+                post_has_article: 1
+            },
+            {
+                post_type: 'img',
+                post_content: 'http://66.media.tumblr.com/0fc8eb068e3b3505e93d531934df9ee4/tumblr_o3xgaegrev1tm0l12o1_500.jpg',
+                post_date: '2016-07-31 14:16:15',
+                post_like_count: 0,
+                post_comment_count: 0,
+                post_has_article: 1
+            },
+            {
+                post_type: 'video',
+                post_content: '4O9OJJUrDOE',
+                post_date: '2016-07-31 14:16:15',
+                post_like_count: 954,
+                post_comment_count: 524,
+                post_has_article: 1
+            }
+        ]
     });
 });
 
--- app/models/database/posts/insert_post.js
+++ app/models/database/posts/insert_post.js
@@ -2,10 +2,12 @@ module.exports = {
     insert_post: function(post_details, callback) {
         if (typeof(callback) === 'undefined') callback = function() {};
 
+        if (!('title' in post_details)) post_details.title = '';
+
         if ('author' in post_details && 'date' in post_details && 'content' in post_details && 'type' in post_details) {
             this.conn.query(
-                'INSERT INTO posts (author_ID, post_date, post_content, post_type) VALUES(?, ?, ?, ?);', 
-                [post_details.author, post_details.date, post_details.content, post_details.type],
+                'INSERT INTO posts (author_ID, post_date, post_content, post_type, post_title) VALUES(?, ?, ?, ?, ?);', 
+                [post_details.author, post_details.date, post_details.content, post_details.type, post_details.title],
                 function(err, result) {
                     if (err) throw err;
                     callback();
