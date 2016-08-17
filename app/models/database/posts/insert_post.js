6c4ca38f3c89656cf4d8c013470ee795455644d2
GeorgeGks <GeorgeGks@users.noreply.github.com>
2016-08-17 15:56:00 +0300

    Admin panel update

--- app/models/database/posts/insert_post.js
+++ app/models/database/posts/insert_post.js
@@ -7,13 +7,12 @@ module.exports = {
         if (!('status' in post_details)) post_details.status = 'draft';
         if (!('has_article' in post_details)) post_details.has_article = 0;
         if (!('article_content' in post_details)) post_details.article_content = '';
-        if (!('alt' in post_details)) post_details.alt = '';
 
         if ('author' in post_details && 'date' in post_details && 'post_content' in post_details && 'type' in post_details && 'title' in post_details) {
             this.pool.getConnection(function(err, connection) {
                 connection.query(
-                    'INSERT INTO posts (author_email, post_date, post_content, post_type, post_title, post_status, post_has_article, article_content, alt) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?);', 
-                    [post_details.author, post_details.date, post_details.post_content, post_details.type, post_details.title, post_details.status, post_details.has_article, post_details.article_content, post_details.alt],
+                    'INSERT INTO posts (author_email, post_date, post_content, post_type, post_title, post_status, post_has_article, article_content) VALUES(?, ?, ?, ?, ?, ?, ?, ?);', 
+                    [post_details.author, post_details.date, post_details.post_content, post_details.type, post_details.title, post_details.status, post_details.has_article, post_details.article_content],
                     function(err, result) {
                         if (err) throw err;
                         connection.release();
