aca4a09e0d956b4274691c7422b4faac48c34e4e
GeorgeGkas <georgegkas@gmail.com>
2017-10-19 23:14:04 +0300

    Added more pending tests to database model.

--- test/unit/specs/models/database/posts/insert_post.js
+++ test/unit/specs/models/database/posts/insert_post.js
@@ -1,3 +1,10 @@
 describe('insert_post()', function () {
-
+  it('should return error if `post_details` is not object.');
+  it('should return error if either `author`, `post_content`, `type`, or `title` is not present.');
+  it('`post_details.status` should accept only spessific values.');
+  it('`post_details.status` should be set to `draft` if is not present.');
+  it('`post_details.has_article` should accept spessific values.');
+  it('`post_details.has_article` should be set to `0` if is not present.');
+  it('`post_details.article_content` should be set to empty string if is not present.');
+  it('`post_details.type` should accept only spessific values.');
 });
--- test/unit/specs/models/database/subscriptions/update_subscription.js
+++ test/unit/specs/models/database/subscriptions/update_subscription.js
@@ -1,3 +1,5 @@
 describe('update_subscription()', function () {
-
+  it('should return error if `sub_details` is not object.');
+  it('should return error if `email` is not valid email.');
+  it('should return error if `active` is neither 0 nor 1.');
 });
