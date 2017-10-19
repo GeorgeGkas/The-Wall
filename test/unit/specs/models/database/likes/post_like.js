afc897180b4debebb6225222c519127901a0c613
GeorgeGkas <georgegkas@gmail.com>
2017-10-19 10:02:00 +0300

    Added more pending tests to likes model.

--- test/unit/specs/models/database/likes/post_like.js
+++ test/unit/specs/models/database/likes/post_like.js
@@ -1,4 +1,7 @@
 describe('post_like()', function () {
+  it('should return error if `post_id` is not string.');
   it('should return error if `post_id` is not provided.');
+  it('should update `post_like_count`.');
+  it('should add more points to `post_feature_dynamic`.');
   it('should succeed.');
 });
