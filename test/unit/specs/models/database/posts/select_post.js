1ac8fbd3cf7829b53cc59b37cda5da8b708c7aeb
GeorgeGkas <georgegkas@gmail.com>
2017-10-20 15:59:25 +0300

    Added more pending tests.

--- test/unit/specs/models/database/posts/select_post.js
+++ test/unit/specs/models/database/posts/select_post.js
@@ -1,3 +1,9 @@
 describe('select_post()', function () {
-
+  it('should return error if neither string nor object is provided.');
+  it('should return featured post if string `featured` is past as argument.');
+  it('should return error if object contains other properties than `type`, `status`, `author`, `title`.');
+  it('should return error if `type` is neither `img`, `quote` nor `video`.');
+  it('should return error if `status` is neither `draft`, `published` nor `deleted`.');
+  it('should return error if `author` is not valid email.');
+  it('should return limit number of posts if `limit` property is provided,');
 });
--- test/unit/specs/models/database/posts/update_post.js
+++ test/unit/specs/models/database/posts/update_post.js
@@ -1,3 +1,12 @@
 describe('update_post()', function () {
-
+  it('should return error if `update_details` is neither object nor string.');
+  it('should return error if `post_id` is not present.');
+  it('`update_details.status` should accept only spessific values.');
+  it('`update_details.has_article` should accept spessific values.');
+  it('should update `post_content`.');
+  it('should update `article_content`.');
+  it('should update `title`.');
+  it('`post_details.type` should accept only spessific values.');
+  it('should increase post view count if `add-one-view` is used by update_details.');
+  it('should increase post view count if `add-one-view` should be followed by post_id.');
 });
